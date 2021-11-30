d55ab367ec3b4e8bd1c5fa926f531c94e1e17f0a
auswitch <sawit_cha@cmu.ac.th>
2021-11-30 16:49:12 +0700

    lab7_3_640610672 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,21 @@
-#include<iostream>
-
-using namespace std;
-
-
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+int adiff(int A,int B){
+  int C;
+  while(A > 360){
+    A -= 360;
+  }
+  while(B > 360){
+    B -= 360;
+  }
+  if(A > B){
+    C = A - B;
+  }else{
+    C = B - A;
+  }
+  while(C >= 360){
+    C -= 360;
+  }
+  if(C>180){
+    C = 360 - C;
+  }
+  return C ;
 }
